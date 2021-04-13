9dcb560a0ebec2194c76d90e5e64a59271c24760
Frank Anthony Leyva Gomez <leyva.gomez.frank@gmail.com>
2021-04-13 17:32:10 -0500

    sss

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY =config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1","herokuapp.com"]
+ALLOWED_HOSTS = ["*","herokuapp.com"]
+# 127.0.0.1
 
 
 # Application definition
